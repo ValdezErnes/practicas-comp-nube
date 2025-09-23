eb1f31037e3adc355d965d308da6b09b6ee6b93d
ValdezernesPracticas <Valdezernesto801@gmail.com>
2025-09-23 10:27:29 -0700

    practica 05

---
+++ practica_05/lib/src/slider_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  State<SliderPage> createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double valor = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider Page'),
+        centerTitle: true,
+        backgroundColor: Colors.green[300],
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            width: 400,
+            color: Colors.black54,
+            child: Slider(
+              value: valor,
+              min: 0,
+              max: 400,
+              label: valor.round().toString(),
+              divisions: 100,
+              onChanged: (value) {
+                setState(() {
+                  valor = value;
+                });
+              },
+            ),
+          ),
+          Container(
+            height: 400,
+            width: 580,
+            color: Colors.black54,
+            child: Center(
+              child: Image(
+                image: AssetImage('assets/michi.jpg'),
+                width: valor,
+                height: valor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
