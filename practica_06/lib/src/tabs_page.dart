21e42a79d1ad839cab1a8e942f37d754468c0052
ValdezernesPracticas <Valdezernesto801@gmail.com>
2025-09-28 10:08:08 -0700

    practica 06

---
+++ practica_06/lib/src/tabs_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.tealAccent,
+          title:  Text("Tabs Page"),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.accessibility_new)),
+              Tab(icon: Icon(Icons.mail)),
+              Tab(icon: Icon(Icons.map)),
+              Tab(icon: Icon(Icons.access_alarm)),
+            ],
+          ),
+        ),
+        body: TabBarView(children: [
+          Icon(Icons.accessibility_new),
+          Icon(Icons.mail),
+          Icon(Icons.map),
+          Icon(Icons.access_alarm)
+        ],),
+      ),
+    );
+  }
+}
